61fd2d8512ad83afdeac2ad9d3611968172abcca
ino <amourssou@gmail.com>
2024-12-26 12:47:09 +0000

    fix bugs
    add CustomInputFormatterPlugin class

--- android/app/src/main/kotlin/com/mankou/custom_input_formatter/CustomInputFormatterPlugin.kt
+++ android/app/src/main/kotlin/com/mankou/custom_input_formatter/CustomInputFormatterPlugin.kt
@@ -2,4 +2,4 @@ package com.mankou.custom_input_formatter.custom_input_formatter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class CustomInputFormatterPlugin: FlutterActivity()
